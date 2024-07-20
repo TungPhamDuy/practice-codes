fea5c5483139b1758d279ecb4c066622814889e1
TungPhamDuy <phamduytung999@gmail.com>
2024-07-20 19:11:28 +0700

    Updated function

--- practice.py
+++ practice.py
@@ -1,1 +1,8 @@
-print(list(range(0,-5)))
+book_title =  ['great', 'expectations','the', 'adventures', 'of', 'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
+book_count = {}
+for i in book_title:
+    if i not in book_count:
+        book_count[i]=1
+    else:
+        book_count[i]+=1
+print(book_count)
