14c81d1badbe6837585fbddd92ae96bf3f96dff8
TungPhamDuy <phamduytung999@gmail.com>
2024-07-25 21:28:21 +0700

    Updated function

--- practice.py
+++ practice.py
@@ -1,3 +1,8 @@
-from time import time
-test = sleep(10)
-print(test)
+import numpy as np
+import math
+l = [5,6,7]
+
+output = [math.e**i for i in l]
+print(sum(output))
+#output = np.exp(l)
+#print(list(output))
